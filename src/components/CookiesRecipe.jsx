bfe3d60b1bd857acd26ad16fd159a05f99eef402
Figby222 <figby222@outlook.com>
2024-06-24 20:11:06 -0700

    Add CookiesRecipe component (Super Cookies)

---
+++ src/components/CookiesRecipe.jsx
@@ -0,0 +1,23 @@
+function CookiesRecipe() {
+    return (
+        <>
+            <h2>Super Cookies</h2>
+            <ul>
+                <li>
+                    1,000 planets of sugar
+                </li>
+                <li>
+                    1,000,000 cups whey protein
+                </li>
+                <li>
+                    2 star systems flour
+                </li>
+                <li>
+                    1 space ship butter
+                </li>
+            </ul>
+        </>
+    );
+}
+
+export default CookiesRecipe;
