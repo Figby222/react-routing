1f8f7d578f50ed783a037cc1e0f35eb55166769a
Figby222 <figby222@outlook.com>
2024-06-24 17:43:10 -0700

    Profile: render DefaultProfile as default Outlet

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import Profile from "./components/Profile.jsx";
+import DefaultProfile from './components/DefaultProfile.jsx';
 import Spinach from "./components/Spinach.jsx";
 import Popeye from "./components/Popeye";
 import './index.css'
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
     path: "profile",
     element: <Profile />,
     children: [
+      { index: true, element: <DefaultProfile /> },
       { path: "spinach", element: <Spinach /> },
       { path: "popeye", element: <Popeye /> },
     ],
